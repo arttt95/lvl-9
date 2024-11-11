0fadc6c2f2c0dbaf53c1a9451ee50aaae1303208
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-11 05:20:01 -0300

    11NOV24 - Aula 13 (CL9) - Grupo de routes.

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,87 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.clients');
 });
+
+/////////////
+// PARTE 2 //
+/////////////
+
+///////////////////////////////////
+// Agrupando as Rotas por PREFIX //
+///////////////////////////////////
+
+//Route::view('/', 'welcome');
+
+// Route::prefix('admin')->group(function(){
+
+//     Route::get('/dashboard', function(){
+//         return "dashboard";
+//     })->name('admin.dashboard');
+    
+//     Route::get('/users', function(){
+//         return "users";
+//     })->name('admin.users');
+    
+//     Route::get('/clients', function(){
+//         return "clients";
+//     })->name('admin.clients');
+
+// });
+
+/////////////////////////////////
+// Agrupando as Rotas por NAME //
+/////////////////////////////////
+
+// Route::name('admin.')->group(function(){
+
+//     Route::get('admin/dashboard', function(){
+//         return "dashboard";
+//     })->name('dashboard');
+    
+//     Route::get('admin/users', function(){
+//         return "users";
+//     })->name('users');
+    
+//     Route::get('admin/clients', function(){
+//         return "clients";
+//     })->name('clients');
+
+// });
+
+//////////////////////////////////////////
+// Agrupando as Rotas por NAME e PREFIX //
+//////////////////////////////////////////
+
+Route::group([
+
+        'prefix' => 'admin',
+        'as' => 'admin.' 
+
+    ], function(){
+
+    Route::get('dashboard', function(){
+        return "dashboard";
+    })->name('dashboard');
+    
+    Route::get('users', function(){
+        return "users";
+    })->name('users');
+    
+    Route::get('clients', function(){
+        return "clients";
+    })->name('clients');
+
+});
+
+
+
+
+
+/////////////
+// PARTE 1 //
+/////////////
 
 // Route::get('/empresa', function() {
 //     return view('site/empresa');
@@ -24,25 +103,25 @@ Route::get('/', function () {
 // Refactoring da rota /empresa que retorna apenas a view empresa
 // A view emprea está localizada em resources/views/site/empresa.blade.php
 
-Route::any('/any', function() {
-    return "Permite todo tipo de acesso http (put, delete, get, post)";
-});
+// Route::any('/any', function() {
+//     return "Permite todo tipo de acesso http (put, delete, get, post)";
+// });
 
-Route::match(['put', 'delete'],'/match', function() {
-    return "Permite apenas acessos definidos";
-});
+// Route::match(['put', 'delete'],'/match', function() {
+//     return "Permite apenas acessos definidos";
+// });
 
-Route::get('produto/{id}/{categoria?}', function($id, $cat = 'Default') {
-    return "O id do produto é: ".$id." e a categoria é: ".$cat;
-});
+// Route::get('produto/{id}/{categoria?}', function($id, $cat = 'Default') {
+//     return "O id do produto é: ".$id." e a categoria é: ".$cat;
+// });
 
-Route::redirect('/sobre', '/empresa');
-Route::view('/empresa', 'site/empresa');
+// Route::redirect('/sobre', '/empresa');
+// Route::view('/empresa', 'site/empresa');
 
-Route::get('/timesnownews', function(){
-    return view('/news');
-})->name('noticias');
+// Route::get('/timesnownews', function(){
+//     return view('/news');
+// })->name('noticias');
 
-Route::get('/novidades', function(){
-    return redirect()->route('noticias');
-});
+// Route::get('/novidades', function(){
+//     return redirect()->route('noticias');
+// });
